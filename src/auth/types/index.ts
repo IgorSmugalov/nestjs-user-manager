c1ecc0566827c31ee434ab8da5ba38afce2dc59b
SmugalovIgor <smugalovigor@gmail.com>
2023-06-18 17:59:51 +0300

    Update access jwt claims for flat structure, update jwt's dto

--- src/auth/types/index.ts
+++ src/auth/types/index.ts
@@ -2,8 +2,10 @@ import { UserAuthData } from '@prisma/client';
 import * as jose from 'jose';
 
 type BasicAccessJwtClaims = Pick<jose.JWTPayload, 'exp'>;
+
 export type AuthUserAccessJwtClaims = Pick<
   UserAuthData,
-  'userProfileId' | 'activated'
+  'id' | 'userProfileId' | 'activated'
 >;
+
 export type AccessJwtClaimsSet = BasicAccessJwtClaims & AuthUserAccessJwtClaims;
